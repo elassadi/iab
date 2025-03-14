312bb44e5a48c9b3a1370a083d5c8f91b27e6008
Mohamed Elassadi <mohamed.elassadi@gmail.com>
2025-03-14 14:51:54 +0100

    add changes

--- change.rb
+++ change.rb
@@ -22,6 +22,45 @@ def base64_encode_file(file_path)
   puts "Base64 encoded file saved as: #{new_file_path}"
 end
 
-# Example usage
+# Function to decode a Base64 file back to its original binary form
+def base64_decode_file(encoded_file_path)
+  unless File.exist?(encoded_file_path)
+    puts "Error: File not found!"
+    return
+  end
 
-  base64_encode_file("cm.yaml")
+
+  puts "Encoded file: #{encoded_file_path}"
+  # Read the Base64-encoded content
+  base64_content = File.read(encoded_file_path)
+
+  puts "Base64 content: #{base64_content.length}"
+
+  # Decode it back to binary
+  binary_content = Base64.decode64(base64_content)
+
+  # Generate original file name (remove .64 extension)
+  original_file_path = encoded_file_path.sub(/\.64$/, '')
+
+  # Save decoded binary content
+  File.binwrite(original_file_path+".decode", binary_content)
+
+  puts "Decoded binary file saved as: #{original_file_path}"
+end
+
+# Example usage
+if ARGV.length < 2
+  puts "Usage: ruby script.rb encode|decode <file_path>"
+else
+  action = ARGV[0].downcase
+  file_path = ARGV[1]
+
+  case action
+  when "encode"
+    base64_encode_file(file_path)
+  when "decode"
+    base64_decode_file(file_path)
+  else
+    puts "Invalid action. Use 'encode' or 'decode'."
+  end
+end
